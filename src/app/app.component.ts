ddd409804e06cc1292e106fa0ca3f97971c49bc3
worldcoolguy <miguelsantiagoupwork925@gmail.com>
2017-03-26 20:37:31 +0200

    Main component refactor with routing and hello component as static component. Also including general styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,12 @@
-import {Component} from '@angular/core';
+import {Component, ViewEncapsulation, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app',
   templateUrl: './app.component.html',
+  encapsulation: ViewEncapsulation.None,
+  styleUrls: [
+    'app.scss'
+  ]
 })
 export class AppComponent {
 }
