4e1ae4d14e40d73ae691da8b777e8fddcfba6630
worldcoolguy <miguelsantiagoupwork925@gmail.com>
2017-07-17 17:07:27 +0200

    Cleaned up module.ts file and generating hello component and background directive.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,62 +1,24 @@
-import { NgModule} from '@angular/core'
 import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpModule } from '@angular/http';
+import { NgModule } from '@angular/core';
 
 import { rootRouterConfig } from './app.routes';
 import { AppComponent } from './app.component';
-
-import { ChartsModule } from 'ng2-charts';
-
-import { BackgroundDirective} from './background/background.directive';
-
 import { HelloComponent } from './hello/hello.component';
-import { WorkComponent } from './work/work.component';
-import { ProjectService } from './work/project.service';
-
-import { ProjectComponent, ProjectInfoComponent } from './work/project.component';
-import { IpscComponent } from './ipsc/ipsc.component';
-import { GalleryComponent } from './ipsc/gallery.component';
-import { RunningComponent,
-         EndomondoLastActivity,
-         RunningHighscores,
-         RunningOfficial,
-         RunningGraphHistory} from './running/running.component';
-import { RunningPipe, HighscorePipe, OfficialPipe, CountPipe, TotalKMPipe } from './running/running.pipe';
-
+import { BackgroundDirective } from './background.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BackgroundDirective,
     HelloComponent,
-    WorkComponent,
-    ProjectComponent,
-    ProjectInfoComponent,
-    IpscComponent,
-    GalleryComponent,
-    RunningComponent,
-    RunningGraphHistory,
-    RunningOfficial,
-    RunningHighscores,
-    EndomondoLastActivity,
-    RunningPipe,
-    HighscorePipe,
-    OfficialPipe,
-    CountPipe,
-    TotalKMPipe
+    BackgroundDirective
   ],
   imports: [
     BrowserModule,
-    HttpModule,
     RouterModule.forRoot(rootRouterConfig, { useHash: false }),
-    ChartsModule,
 
   ],
-  providers: [
-    ProjectService,
-
-  ],
-  bootstrap: [ AppComponent ]
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,16 +5,12 @@ import { WorkComponent } from './work/work.component';
 import { IpscComponent } from './ipsc/ipsc.component';
 import { RunningComponent} from './running/running.component';
 import { ProjectInfoComponent} from './work/project.component';
-import { RepoBrowserComponent } from './github/repo-browser/repo-browser.component';
-import { RepoListComponent } from './github/repo-list/repo-list.component';
-import { RepoDetailComponent } from './github/repo-detail/repo-detail.component';
-import { ContactComponent } from './contact/contact.component';
 
 export const rootRouterConfig: Routes = [
-  { path: '', component: HelloComponent},
-  { path: 'latest-work', component: WorkComponent},
-  { path: 'latest-work/:name', component: WorkComponent},
+  { path: '', component: HelloComponent}
+  /*{ path: 'latest-work', component: WorkComponent},
+  ({ path: 'latest-work/:name', component: WorkComponent},
   { path: 'running', component: RunningComponent },
-  { path: 'ipsc', component: IpscComponent }
+  { path: 'ipsc', component: IpscComponent }*/
 ];
 
